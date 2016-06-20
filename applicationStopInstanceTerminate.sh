2f45b8fe0f2a5ca076f22e8c72cb6524b8a3c419
elakkad <cooky9977@yahoo.com>
2016-06-20 10:33:31 -0700

    Update applicationStopInstanceTerminate.sh

--- applicationStopInstanceTerminate.sh
+++ applicationStopInstanceTerminate.sh
@@ -1,1 +1,2 @@
-
+[ -f /var/deployments/dockerExperiment.jar ]  && echo "destroying VM" || echo "no previous deployments"
+[ -f /var/deployments/dockerExperiment.jar ] && `shutdown now` || echo "no previous deployments"
