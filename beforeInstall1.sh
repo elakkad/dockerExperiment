cdfb716320aa8cc3d5004ad878cae44c5c5c1bc1
elakkad <cooky9977@yahoo.com>
2016-06-21 13:44:33 -0700

    Update beforeInstall1.sh

--- beforeInstall1.sh
+++ beforeInstall1.sh
@@ -1,2 +1,2 @@
 uptime
-[ -f /var/deployments/dockerExperiment.jar ]  && shutdown || echo "no previous deployments"
+#[ -f /var/deployments/dockerExperiment.jar ]  && shutdown || echo "no previous deployments"
